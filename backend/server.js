e89b2bba65ac2f803277a5c8bdc2c453ae0bc3fd
LINUS KIBET <122885970+devlinuskibet@users.noreply.github.com>
2025-07-02 14:15:46 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -176,4 +176,5 @@ app.delete('/api/contact/:id', verifyAdminToken, async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log('Server running on port 5000')); 
+//app.listen(5000, () => console.log('Server running on port 5000')); 
+ app.listen(process.env.PORT || 5000, () => console.log('Server running...'));
